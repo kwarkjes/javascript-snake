537a4aa5e41be8510450f821110c047816fa6cad
Yassine Elouazzani <yassine@youngguns.nl>
2013-12-01 00:46:56 +0100

    trigger arrow keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-
+    var keyCodes= {
+        left : 37,
+        up : 38,
+        right : 39,
+        down : 40
+    };
+    $(document).keydown(function(e){
+        if (
+                e.keyCode == keyCodes.left ||
+                e.keyCode == keyCodes.up ||
+                e.keyCode == keyCodes.right ||
+                e.keyCode == keyCodes.down
+        ) {
+           console.log( "control key pressed" );
+           return false;
+        }
+    });
 });
 
