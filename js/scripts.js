f22f79816333ea6c4e68edcecfeea78b77e3e649
Yassine Elouazzani <yassine@youngguns.nl>
2013-12-01 02:52:14 +0100

    code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,35 @@ var snakeSpeed = 200;
 
 $(document).ready(function() {
     snakeShape = $('.game-window span');
-    var keyCodes= {
+    var keyCodes = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down',
         32: 'spacebar'
     };
-    $(document).keydown(function(e){
+
+    $(document).keydown(function(e) {
         var key = keyCodes[e.keyCode];
         if (key) {
-           if (key === 'spacebar') {
-               if ($('body').hasClass('pause') && activeDirection !== null) {
+            if (key === 'spacebar') {
+                if ($('body').hasClass('pause') && activeDirection !== null) {
                     animateSnakeShape(activeDirection);
-               }else {
-                   snakeShape.stop();
-               }
-               $('body').toggleClass('pause');
-           }
-           else {
-               if (!$('body').hasClass('pause')) {
+                } else {
+                    snakeShape.stop();
+                }
+                $('body').toggleClass('pause');
+            }
+            else {
+                if (!$('body').hasClass('pause')) {
                     activeDirection = key;
-                    dump(activeDirection);
                     animateSnakeShape(activeDirection);
-               }
-           }
-           return false;
+                }
+            }
+            return false;
         }
     });
-//    dump($('.game-window span').position());
+
 });
 
 function dump(data) {
@@ -40,20 +40,20 @@ function dump(data) {
 function animateSnakeShapehotizontal() {
     var old_position = snakeShape.position();
     snakeShape.stop().animate({
-        left : ((activeDirection === 'left' ? old_position.left - 10 : old_position.left + 10))+'px'
-    },snakeSpeed,animateSnakeShapehotizontal);
+        left: ((activeDirection === 'left' ? old_position.left - 10 : old_position.left + 10)) + 'px'
+    }, snakeSpeed, animateSnakeShapehotizontal);
 }
 function animateSnakeShapevertical() {
     var old_position = snakeShape.position();
     snakeShape.stop().animate({
-        top : ((activeDirection === 'up' ? old_position.top - 10 : old_position.top + 10))+'px'
-    },snakeSpeed,animateSnakeShapevertical);
+        top: ((activeDirection === 'up' ? old_position.top - 10 : old_position.top + 10)) + 'px'
+    }, snakeSpeed, animateSnakeShapevertical);
 }
 function animateSnakeShape(direction) {
-    if (direction  === 'left' || direction  === 'right') {
+    if (direction === 'left' || direction === 'right') {
         animateSnakeShapehotizontal();
     }
-    if (direction  === 'up' || direction  === 'down') {
+    if (direction === 'up' || direction === 'down') {
         animateSnakeShapevertical();
     }
 }
